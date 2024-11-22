dc56da235427545dd481ae2ff0dd05284011bef3
sicks <tcopeland@dg3n.net>
2024-11-22 00:31:55 -0800

    hook up basic error handling

--- app/controllers/kitchen_controller.rb
+++ app/controllers/kitchen_controller.rb
@@ -1,5 +1,6 @@
 class KitchenController < ApplicationController
   allow_unauthenticated_access
+  before_action :simulate_error
 
   def show
   end
@@ -7,5 +8,21 @@ class KitchenController < ApplicationController
   def create
     type = params[:commit]
     flash.now[type] = "#{type.capitalize} type flash message"
+  end
+
+  private
+
+  def simulate_error
+    case params[:id]
+    when "not_found"
+      raise ActiveRecord::RecordNotFound
+    when "bad_request"
+      raise ActionController::BadRequest
+    end
+
+    case params[:commit]
+    when "error"
+      raise ActiveRecord::RecordInvalid
+    end
   end
 end
